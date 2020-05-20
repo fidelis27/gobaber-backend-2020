1b39a3c5832beca377973110b07c02a47d9e618c
unknown <thiago.fidelis27@gmail.com>
2020-05-20 17:03:41 -0300

    Primeiro projeto tsc e conceitos arquitetura

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import appointmentsRouter from './appointments.routes';
+
+const routes = express.Router();
+
+routes.use('/appointments', appointmentsRouter);
+
+export default routes;
